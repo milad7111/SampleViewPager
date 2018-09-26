8360636387ae4139c087cefdd27a2c131c3a42e0
morteza.hosseinee <mortezahosseinee@gmail.com>
2018-09-26 11:17:19 +0330

    First Commit

---
+++ app/src/main/java/com/example/sampleviewpager/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.sampleviewpager;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
+import com.tbuonomo.viewpagerdotsindicator.SpringDotsIndicator;
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+//        DotsIndicator dotsIndicator = findViewById(R.id.dots_indicator);
+//        SpringDotsIndicator springDotsIndicator = findViewById(R.id.spring_dots_indicator);
+        WormDotsIndicator wormDotsIndicator = findViewById(R.id.worm_dots_indicator);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter();
+        viewPager.setAdapter(adapter);
+        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+//        dotsIndicator.setViewPager(viewPager);
+//        springDotsIndicator.setViewPager(viewPager);
+        wormDotsIndicator.setViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/example/sampleviewpager/ViewPagerAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.sampleviewpager
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import java.util.*
+
+class ViewPagerAdapter : PagerAdapter() {
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val item = LayoutInflater.from(container.context).inflate(if (position == count - 1) R.layout.al else R.layout.material_page, container, false)
+
+        container.addView(item)
+        return item
+
+        //        CardView cardView = item.findViewById(R.id.card_view);
+        //        cardView.setCardBackgroundColor(ContextCompat.getColor(container.getContext(), (items.get(getRelativePosition(position)).color)));
+        //        return super.instantiateItem(container, getRelativePosition(position));
+    }
+
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+        //        super.destroyItem(container, getRelativePosition(position), object);
+    }
+
+    private class Item(private val color: Int)
+
+    companion object {
+
+        private val items = Arrays.asList(
+                Item(R.color.md_indigo_500),
+                Item(R.color.md_green_500),
+                Item(R.color.md_red_500),
+                Item(R.color.md_orange_500),
+                Item(R.color.md_purple_500))
+    }
+}
